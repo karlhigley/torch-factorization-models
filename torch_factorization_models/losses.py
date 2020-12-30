5b37ebb670bd69af88510ab17854a6ca0dc3e881
Karl Higley <kmhigley@gmail.com>
2020-12-30 12:14:45 -0500

    Add doc-strings to losses

--- torch_factorization_models/losses.py
+++ torch_factorization_models/losses.py
@@ -12,6 +12,19 @@ class LossFunction(enum.Enum):
 
 
 def select_loss(name, num_items, margin=0.1):
+    """Select/construct a loss function by name
+
+    Args:
+        name (string): Short name of the desired loss
+        num_items (int): Total number of items in the dataset (used for WARP loss)
+        margin (float, optional): Margin for hinge and WARP loss. Defaults to 0.1.
+
+    Raises:
+        ValueError: When name doesn't correspond to a known loss function
+
+    Returns:
+        function: Loss function for corresponding positive and negative examples
+    """
     enum = LossFunction(name)
     if enum == LossFunction.BCE:
         return binary_cross_entropy_loss
@@ -26,21 +39,75 @@ def select_loss(name, num_items, margin=0.1):
 
 
 def _stable_bce(logits, label):
+    """Numerically stable implementation of binary cross-entropy
+
+    This avoids problems at the boundaries when the output of a sigmoid function is
+    passed into a log.
+
+    Args:
+        logits (th.Tensor): Model outputs before activation function
+        label (float): Label for the corresponding examples
+
+    Returns:
+        th.Tensor: Binary cross-entropies computed from logits and label
+    """
     return th.clamp(logits, min=0) - logits * label + th.log1p(th.exp(-th.abs(logits)))
 
 
 def binary_cross_entropy_loss(pos_preds, neg_preds):
+    """Binary cross-entropy loss for paired positive and negative examples
+
+    Args:
+        pos_preds (th.Tensor): Logits for positive examples
+        neg_preds (th.Tensor): Logits for negative examples
+
+    Returns:
+        th.Tensor: Computed losses for each example pair
+    """
     losses = _stable_bce(pos_preds, 1.0) + _stable_bce(neg_preds, 0.0)
     return losses
 
 
 def bpr_loss(pos_preds, neg_preds):
+    """Bayesian Personalized Ranking loss for paired positive/negative examples
+
+    This would naively be computed as -th.log(th.sigmoid(pos_preds - neg_preds)),
+    but that has similar numerical instability problems as the naive computation
+    of binary cross-entropy. Since it's equivalent to the positive example term
+    of BCE loss, we can re-use the stable implementation of BCE here and treat
+    each pair of positive and negative predictions as a single positive example.
+
+    Args:
+        pos_preds (th.Tensor): Logits for positive examples
+        neg_preds (th.Tensor): Logits for negative examples
+
+    Returns:
+        th.Tensor: Computed losses for each example pair
+    """
     losses = _stable_bce(pos_preds - neg_preds, 1.0)
     return losses
 
 
 def build_hinge_loss(margin):
+    """Builds a hinge loss function w/ the supplied margin
+
+    Args:
+        margin (float): Hinge loss margin
+    """
+
     def hinge_loss(pos_preds, neg_preds):
+        """Ranking hinge loss
+
+        The computed losses are scaled by the margin so that changing the margin
+        results in losses with (approximately) constant scale.
+
+        Args:
+            pos_preds (th.Tensor): Logits for positive examples
+            neg_preds (th.Tensor): Logits for negative examples
+
+        Returns:
+            th.Tensor: Computed losses for each example pair
+        """
         distances = th.sigmoid(pos_preds) - th.sigmoid(neg_preds)
         raw_losses = th.clamp(margin - distances, min=0)
 
@@ -50,9 +117,30 @@ def build_hinge_loss(margin):
 
 
 def build_warp_loss(num_items, margin):
+    """Builds a WARP loss function w/ supplied margin and number of items
+
+    Args:
+        num_items (int): Total number of items in the dataset
+        margin (float): Hinge loss margin
+
+    Returns:
+        th.Tensor: Computed losses for each example pair
+    """
     hinge_loss = build_hinge_loss(margin)
 
     def warp_loss(pos_preds, neg_preds):
+        """Weighted Approximate Rank Pairwise loss
+
+        The computed losses are scaled by the margin so that changing the margin
+        results in losses with (approximately) constant scale.
+
+        Args:
+            pos_preds (th.Tensor): Logits for positive examples
+            neg_preds (th.Tensor): Logits for negative examples
+
+        Returns:
+            th.Tensor: Computed losses for each example pair
+        """
         raw_losses = hinge_loss(pos_preds, neg_preds)
 
         num_samples = neg_preds.shape[1]
