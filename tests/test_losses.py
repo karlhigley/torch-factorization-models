7312239db390dc8327f4620b90ea81823e2e4331
Karl Higley <kmhigley@gmail.com>
2021-04-26 16:37:40 -0400

    Fix loss function tests

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,6 +1,10 @@
 import torch as th
 from hypothesis import given
-from torch_factorization_models.losses import bpr_loss, hinge_loss, warp_loss
+from torch_factorization_models.losses import (
+    bpr_loss,
+    build_hinge_loss,
+    build_warp_loss,
+)
 
 from tests.conftest import raw_predictions
 
@@ -9,7 +13,9 @@ from tests.conftest import raw_predictions
 def test_hinge_loss_range(raw_predictions):
     positives, negatives = raw_predictions
 
-    loss = hinge_loss(th.tensor(positives), th.tensor(negatives), margin=1.0).mean()
+    hinge_loss = build_hinge_loss(1.0)
+
+    loss = hinge_loss(th.tensor(positives), th.tensor(negatives)).mean()
     assert (loss >= 0.0).all()
     assert (loss <= 2.0).all()
 
@@ -26,7 +32,7 @@ def test_bpr_loss_range(raw_predictions):
 def test_warp_loss_range(raw_predictions):
     positives, negatives = raw_predictions
 
-    loss = warp_loss(
-        th.tensor(positives), th.tensor([negatives, negatives]), 1000
-    ).mean()
+    warp_loss = build_warp_loss(1000, 0.1)
+
+    loss = warp_loss(th.tensor(positives), th.tensor([negatives, negatives])).mean()
     assert (loss >= 0.0).all()
